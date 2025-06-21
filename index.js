373a40a8310da5bc7b6dda02983325a00fbcea09
Aniruddha Bhandare <anirudhabhandare4303@gmail.com>
2025-06-21 23:16:35 +0530

    fix: use process.env.PORT for Render

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/items", itemRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("✅ Server running on port 5000")))
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+  })
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));

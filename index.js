1165bb67b4695f805029070e1b3ca84aa19a0d31
Aniruddha Bhandare <anirudhabhandare4303@gmail.com>
2025-06-21 23:12:04 +0530

    hello

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const itemRoutes = require("./routes/itemRoutes");
+
 dotenv.config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/items", itemRoutes);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
-  .catch((err) => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("✅ Server running on port 5000")))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
